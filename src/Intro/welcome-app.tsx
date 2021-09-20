c3e3d8b0d4fded94a9ea8f10d270e62eba60ecc4
Shaozhe Li <lsz@udel.edu>
2021-09-19 21:59:39 -0400

    follow the class and learning resource to learn the function and object(adder) of typescript

--- src/Intro/welcome-app.tsx
+++ src/Intro/welcome-app.tsx
@@ -1,4 +1,23 @@
 
+
+function adder(first: number, second:number): number{
+    return first + second;    
+}
+
+adder(1, 2)
+
+function adderObj({first,second}: {first: number,second:number}): number{
+    return first + second;
+}
+
+
+function adderData(data: {first: number, second: number}): number{
+    return data.first+data.second;
+}
+
+adderObj({first: 1,second: 2})
+adderData({first: 1, second: 2})
+
 export function Welcome (): JSX.Element{
     return(
     <div>Welcome to Shaozhe Li's APP!</div>

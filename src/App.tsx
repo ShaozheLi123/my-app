2f5531e1e2a7d24ca9c4e69df4bc5f5ff163935a
Shaozhe Li <lsz@udel.edu>
2021-09-20 15:20:54 -0400

    set up the my own wabpage-app

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,12 @@ import {Welcome} from './Intro/welcome-app';
 import {Person} from "./interface/person";
 
 function App(): JSX.Element{
-  const message: string = "Hello world!";
-  const shaozhe: Person = {name: "Shaozhe Li", hairColor: "blue"};
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p> 
-        <Welcome person = {shaozhe}></Welcome>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }

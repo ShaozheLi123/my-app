906ba0b905420d6ad8210ce7eade32beee64d003
Shaozhe Li <lsz@udel.edu>
2021-09-19 23:11:44 -0400

    commit the did not saved file

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import {Welcome} from './Intro/welcome-app';
 import {Person} from "./interface/person";
 
 function App(): JSX.Element{
+  const message: string = "Hello world!";
   const shaozhe: Person = {name: "Shaozhe Li", hairColor: "blue"};
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {message}
+        </p> 
         <Welcome person = {shaozhe}></Welcome>
         <a
           className="App-link"

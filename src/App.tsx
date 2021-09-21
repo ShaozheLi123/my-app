f11343aaee7db7608f69307ec8beac016b3bac1b
Shaozhe Li <lsz@udel.edu>
2021-09-20 21:48:00 -0400

    delete the useEffect in import to solve problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Prompt } from './Intro/Prompt';
 import { SwapButton } from './Intro/SwapButton';

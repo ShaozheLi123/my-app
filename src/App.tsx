a2c8863ba0510eb98c656eccb994cf572f4200e1
Shaozhe Li <lsz@udel.edu>
2021-09-22 19:49:51 -0400

    initial the app page o start my app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Prompt } from './Intro/Prompt';
-import { SwapButton } from './Intro/SwapButton';
+
+
 
 function App(): JSX.Element{
-  const [prompt, setPrompt] =useState<string>("Hello and Welcome");
 
-  /*useEffect(() => {
-    setPrompt("Hello world actual")
-  })*/
 
   return (
     <div className="App">
-      <Prompt text = {prompt}></Prompt>
-      <SwapButton change={setPrompt}></SwapButton>
+      
     </div>
     
   );

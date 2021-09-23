672d412dcca044e33628063150a810c9fa752b4b
Shaozhe Li <lsz@udel.edu>
2021-09-22 22:01:36 -0400

    creater viewer and control panel, and try them in function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import CARDS from './assets/cards.json';
+import { CardViewer } from './components/CardViewer';
+import { ControlPanel } from './components/ControlPanel';
 
 
 function App(): JSX.Element{
 
   return (
     <div className="App">
-      {JSON.stringify(CARDS)};
+      <ControlPanel></ControlPanel>
+      <CardViewer></CardViewer>
     </div>
     
   );

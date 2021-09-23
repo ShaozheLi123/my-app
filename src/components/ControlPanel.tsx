1e39a63395a113463460889cb89390bed7c8d29d
Shaozhe Li <lsz@udel.edu>
2021-09-23 19:44:10 -0400

    get save button to save data in local computer

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -6,15 +6,27 @@ import { UserList } from "./UserList";
 import { useState } from "react";
 import { Task as User} from "editable-dnd-list";
 
+export const LOCAL_STORAGE_USERS = 'my-app-users';
 
+export const INITIAL_USERS: User[] = [
+    {id: '1', text: "Shaozhe"},
+    {id: '2', text: "Kaiser"},
+    {id: '3', text: "Joe"},
+  ];
+  
+  export function getLocalStorageUsers(): User[] {
+    let rawUsers: string | null = localStorage.getItem(LOCAL_STORAGE_USERS);
+    if (rawUsers === null) {
+      return [...INITIAL_USERS];
+    } else {
+      return JSON.parse(rawUsers);
+    }
+  }
 
 export function ControlPanel({setCard, reveal, answerRevealed}: 
     {setCard: (c: Card)=>void, reveal: (r:boolean)=>void, answerRevealed: boolean}) : JSX.Element{
-        const [users, setUsers] = useState<Task[]>([
-            {id: '1', text: "Shaozhe"},
-            {id: '2', text: "Kaiser"},
-            {id: '3', text: "Joe"},
-        ]);
+        const [users, setUsers] = useState<User[]>(getLocalStorageUsers());
+
         
     function setRandomCard(){
         reveal(false);
@@ -26,12 +38,16 @@ export function ControlPanel({setCard, reveal, answerRevealed}:
         setUsers([...shuffledUsers]);
     }
 
+    function save() {
+        localStorage.setItem(LOCAL_STORAGE_USERS, JSON.stringify(users));
+    }
+
     return <Col>
     <h1>Control Panel </h1>
     <UserList users={users} setUsers={setUsers}></UserList>
     <Button onClick = {setRandomCard} className = "m-4">Swap Your Card</Button> 
     <Button onClick = {() => reveal(!answerRevealed)} className = "m-4"> Reveal Answer</Button>
     <Button onClick={shuffleUsers} className="m-4">Shuffle Users</Button>
-
+    <Button onClick={save} className="m-4" variant="success">Save</Button>
     </Col>
 }

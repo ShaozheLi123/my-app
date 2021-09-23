9f90f354f7e00a61e5ec8f8933d097be2c863e69
Shaozhe Li <lsz@udel.edu>
2021-09-22 23:35:37 -0400

    use the data(random function) to chaneg the card question randomly

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,10 +1,12 @@
 import {Button, Col} from "react-bootstrap";
 import { Card } from "../interface/card";
+import CARDS from '../assets/cards.json';
+import { getRandomElement } from "../utilities/data";
 
 
 export function ControlPanel({setCard}: {setCard: (c: Card)=>void}) : JSX.Element{
     return <Col>
     <h1>Control Panel </h1>
-    <Button>Swap Card</Button> 
+    <Button onClick = {() => setCard(getRandomElement(CARDS as Card[]))}>Swap Card</Button> 
     </Col>
 }

45d3fe0416e1f8c397d398c36b5cb7b5a8d3b1ae
Shaozhe Li <lsz@udel.edu>
2021-09-30 16:02:42 -0400

    make the background and animation on the page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap';
 import {Card} from './interface/card'
 import { AddCardModal } from './components/AddCardModal';
+import questionmark from './questionmark.png';
 
 function App(): JSX.Element{
     const [activeCard, setActiveCard] = useState<Card>(CARDS[0] as Card);
@@ -21,19 +22,20 @@ function App(): JSX.Element{
     return (
       <div className = "App">
         <header className="App-header">
-        <Container className="App">
-          <Row>
-          <ControlPanel
-          currentCard={activeCard}
-          showAddCardModal={setVisible}
-          deck={deck}
-          setCard={setActiveCard} 
-          reveal = {reveal}
-          answerRevealed = {answerRevealed}></ControlPanel>
-          <CardViewer card= {activeCard} answerRevealed = {answerRevealed}></CardViewer>
-          <AddCardModal visible={visible} setVisible={setVisible} addCard={addCard}></AddCardModal>
-          </Row>
-        </Container>
+          <img src={questionmark} className="App-questionmark" alt="questionmark"/>
+            <Container className="App">
+              <Row>
+              <ControlPanel
+              currentCard={activeCard}
+              showAddCardModal={setVisible}
+              deck={deck}
+              setCard={setActiveCard} 
+              reveal = {reveal}
+              answerRevealed = {answerRevealed}></ControlPanel>
+              <CardViewer card= {activeCard} answerRevealed = {answerRevealed}></CardViewer>
+              <AddCardModal visible={visible} setVisible={setVisible} addCard={addCard}></AddCardModal>
+              </Row>
+            </Container>
       </header>
     </div>
   );
